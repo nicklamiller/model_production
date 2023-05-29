49d7827db2143b4041a0cea94b57d9a05e7800ed
nicklamiller <nicklamiller@gmail.com>
2023-05-28 18:25:10 -0700

    add model loading logic to app

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,5 +1,6 @@
 import json
 import flask
+import pickle
 
 from flask import Flask, jsonify, request
 
@@ -7,8 +8,21 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    response = json.dumps({'response': 'yahhhh!'})
+    x = 5.963
+    model = load_models()
+    prediction = model.predict([[x]])[0]
+
+    response = json.dumps({'response': prediction})
     return response, 200
+
+
+def load_models():
+    file_name = "../models/model_file.p"
+    with open(file_name, 'rb') as pickled:
+        data = pickle.load(pickled)
+        model = data['model']
+    return model
+
 
 if __name__ == '__main__':
      application.run(debug=True)
