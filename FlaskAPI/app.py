84ada26cd821e5a5db559dee032bb536c3d5e202
nicklamiller <nicklamiller@gmail.com>
2023-05-28 17:53:42 -0700

    add Flask app directory

---
+++ FlaskAPI/app.py
@@ -0,0 +1,15 @@
+import json
+import flask
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    response = json.dumps({'response': 'yahhhh!'})
+    return response, 200
+
+if __name__ == '__main__':
+     application.run(debug=True)
+
